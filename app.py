04d409c8b9cfc64e1a46157fef193fb38a6c1e50
SithuKyaw-AUT <62372448+SithuKyaw-AUT@users.noreply.github.com>
2021-11-24 15:27:25 +1300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ st.set_page_config(layout="wide", page_title='An Interactive ML app')
 import pandas as pd
 from load_css import local_css
 
+from sklearn import datasets
+
 # Models import
 from models.DecisionTree import dt_param_selector
 from models.KNearestNeighbors import knn_param_selector
@@ -13,6 +15,7 @@ from models.NaiveBayes import nb_param_selector
 from models.SVC import svc_param_selector
 from models.NeuralNetwork import nn_param_selector
 from models.LogisticsRegression import lr_param_selector
+from models.GradientBoosting import gb_param_selector
 
 # local imports
 import home
@@ -31,44 +34,67 @@ def load_data(uploaded_dataset):
 		data_file = None
 	return data_file
 
+def get_data(name):
+	data = None
+	prj = None
+	if name == 'Iris':
+		data = datasets.load_iris()
+		prj = "Classification with Iris Dataset"
+	elif name == 'Wine':
+		data = datasets.load_wine()
+		prj = "Classification with Wine Dateset"
+	else:
+		data = datasets.load_breast_cancer()
+		prj = "Classification with Breast Cancer Dataset"
+
+	x = data.data
+	y = data.target
+
+	return x, y, prj
+
 
 # Name the project
 prj_name = None
 data_file = None
+data_file_data = None
+data_file_target = None
 
+st.sidebar.header("1. DATA")
 dataset_container = st.sidebar.expander("Configure your dataset", True)
+
 with dataset_container:
-	prj_name = st.text_input('Name your project: ')
+	if st.checkbox("Load sample data ?"):
+		dataset_name = st.selectbox("Choose sample dataset:", ['Iris', 'Breast Cancer Classification', 'Wine'])
+		data_file_data, data_file_target, prj_name = get_data(dataset_name)
+	else:
+		prj_name = st.text_input('Name your project: ')
 
-	# Sidebar options and file upload
-	st.write('Load your csv file to start the app')
-	uploaded_dataset = st.file_uploader(' ', type=['.csv'])
-	data_file = load_data(uploaded_dataset)
+		# Sidebar options and file upload
+		st.write('Load your csv file to start the app')
+		uploaded_dataset = st.file_uploader(' ', type=['.csv'])
+		data_file = load_data(uploaded_dataset)
 
-	if data_file is not None:
-		st.success('successfully uploaded!') 
+		if data_file is not None:
+			st.success('successfully uploaded!') 
+			st.write('Select featurs and target')
+			new_data=st.multiselect("Select feature columns first. NB: Make Sure Target column is selected LAST",data_file.columns)
+			df1=data_file[new_data]
+			#st.sidebar.dataframe(df1)
+			data_file_data=df1.iloc[:,0:-1]
+			data_file_target=df1.iloc[:,-1]
+
 
 # Choose type of analysis
-obj_prj = None
 
-model_train_container = st.sidebar.expander("Train a model", True)
+st.sidebar.header("2. MODELLING")
+with st.sidebar.expander("Split the dataset", False):
+	test_size = st.number_input("Set the Test datase size", 0.20, 0.40, 0.2, 0.1)
+	seed=st.slider('Seed',1,200)
+
+
+model_train_container = st.sidebar.expander("Train a classification model", False)
 with model_train_container:
-	obj_prj = st.radio('Choose objective of the project: ', ['Regression', 'Classification', 'Clustering'])
-
-	if obj_prj=='Regression':
-		algorithm = st.selectbox(
-			"Choose a model",
-			(
-				"Linear Regression",
-				"Random Forest",
-				"Decision Tree",
-				"Hierarchical",
-				"Ensembles",
-				"Neural Networks",
-			),	
-		)
-	elif obj_prj == 'Classification':
-		algorithm = st.selectbox(
+	algorithm = st.selectbox(
 			"Choose a model",
 			(
 				"Logistics Regression",
@@ -78,35 +104,31 @@ with model_train_container:
 				"K-Nearest Neighbors",
 				"Support Vector Machine",
 				"Neural Networks",
-				"Discriminant Analysis"
-			),	
-		)
-	elif obj_prj == 'Clustering':
-		algorithm = st.selectbox(
-			"Choose a model",
-			(
-				"K-Means",
-				"Hierarchical",
-				"Neural Networks",
+				"Gradient Boosting",
 			),	
 		)
 
-
-	if algorithm == "Decision Tree":
-		model = dt_param_selector()
-	elif algorithm == "K-Nearest Neighbors":
-		model = knn_param_selector()
+	if algorithm == "Logistics Regression":
+		model = lr_param_selector()
 	elif algorithm == "Random Forest":
 		model = rf_param_selector()
+	elif algorithm == "Decision Tree":
+		model = dt_param_selector()
+	elif algorithm == "Naive Bayes":
+		model = nb_param_selector()
+	elif algorithm == "K-Nearest Neighbors":
+		model = knn_param_selector()
 	elif algorithm == "Support Vector Machine":
 		model = svc_param_selector()
 	elif algorithm == "Neural Networks":
 		model = nn_param_selector()
-	elif algorithm == "Logistics Regression":
-		model = lr_param_selector()
+	elif algorithm == "Gradient Boosting":
+		model = gb_param_selector()
 
 
-
+st.sidebar.header("3. EVALUATION")
+with st.sidebar.expander("Evaluation Metrics", False):
+	st.multiselect("Select Metrics: ", ['MSE', 'RMSE', 'MAPE', 'SMAPE', 'MAE'])
 
 
 # sidebar navigation
@@ -117,9 +139,9 @@ step = st.sidebar.radio('Select a page:',
 if step == 'Home':
 	home.home()
 elif step == 'EDA':
-	eda.eda(data_file, prj_name)
+	eda.eda(data_file_data, data_file_target, prj_name)
 elif step == 'Model':
-	train_model.build_model(data_file, prj_name)
+	train_model.build_model(data_file_data, data_file_target, test_size, seed, prj_name, model, algorithm)
 elif step == 'Result':
 	result.result(data_file, prj_name)
 elif step == 'Conclusion':
