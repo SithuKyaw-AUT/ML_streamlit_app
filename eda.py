57ca705c7c7996f225ad8fa1b4e21534507f8918
SithuKyaw-AUT <62372448+SithuKyaw-AUT@users.noreply.github.com>
2021-11-24 15:27:27 +1300

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-def eda(df, prj):
+def eda(df_data, df_target, prj):
 	st.title(prj)
 	st.title('Exploratory Data Analysis')
-	if df is None:
+	if df_data is None:
 		st.warning('No file has been uploaded.')
 	else:
-		st.dataframe(df.head())
-		st.write("Shape of the dataset: ", df.shape)
+		st.dataframe(df_data)
+		st.write("Shape of the dataset: ", df_data.shape)
+		st.write("Unique target variable: ", len(np.unique(df_target)))
 		#st.write("Data types: ", df.dtypes)
-		st.write("Summary: ", df.describe().T)
-		st.write("Null values: ", df.isnull().sum())
-		
+		#st.write("Summary: ", df_data.describe().T)
+		#st.write("Null values: ", df_data.isnull().sum())
+		#st.write("Null values: ", df_target.isnull().sum())
+
+		fig = plt.figure()
+		sns.boxplot(data = df_data, orient = 'h')
+		st.pyplot()
+
+		plt.hist(df_data)
+		st.pyplot()
+
